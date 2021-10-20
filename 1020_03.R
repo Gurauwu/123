6133b70c8308a8dc12c2ff3d681af96c2348704f
Gurauwu <92436534+Gurauwu@users.noreply.github.com>
2021-10-20 20:56:15 +0800

    1020

---
+++ 1020_03.R
@@ -0,0 +1,16 @@
+fruits<-c('banana','apple','orange')
+class(fruits)
+typeof(fruits)
+
+numbers<-c(1,2,3)
+class(numbers)
+typeof(numbers)
+
+log_values<-c(TRUE,FALSE,TRUE,FALSE)
+class(log_values)
+typeof(log_values)
+
+tmp1<-C(1,"2")
+tmp2<-C(1,TRUE)
+tmp3<-c("3",3)
+
